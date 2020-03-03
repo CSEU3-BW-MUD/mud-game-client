34dfd1776f9deab40a150df2e4c78077b40d5796
Rory Flint <roryflintphoto@gmail.com>
2020-03-03 08:54:54 +0000

    Strip out css and replace with themeprovider and global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/globalStyles';
+
+import { theme } from './styles/theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div className="App"></div>
+    </ThemeProvider>
   );
 }
 
