e61b631a09d2dee5cefe2fdd8afe100da4bb9d08
Rory Flint <roryflintphoto@gmail.com>
2020-03-03 10:02:49 +0000

    Add useState to change login/register forms

--- src/views/registerLogin.js
+++ src/views/registerLogin.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+
+import Login from '../components/register-login/login';
+import Register from '../components/register-login/register';
 
 const StyledContainer = styled.div`
   margin: 0 auto;
@@ -7,10 +10,45 @@ const StyledContainer = styled.div`
   width: 50rem;
   height: 40rem;
   border: 3px solid ${props => props.theme.grey};
+
+    .form-container {
+        width: 100%;
+        height: 80%;
+        background-color: red;
+    }
+
+    .buttons {
+        width: 100%;
+        height: 20%;
+        background-color: blue;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-evenly;
+    }
 `;
 
 function RegisterLogin() {
-  return <StyledContainer></StyledContainer>;
+    const [register, setRegister] = useState(false);
+
+    const changeForm = () => {
+        setRegister(!register)
+    }
+
+  return (
+  <StyledContainer>
+      <div className="form-container">
+      {register
+      ? <Register />
+      : <Login />
+      }
+      </div>
+      <div className="buttons">
+    <button>{register ? 'Register' : 'Login'}</button>
+          <button onClick={changeForm}>Change</button>
+      </div>
+  </StyledContainer>
+    );
 }
 
 export default RegisterLogin;
